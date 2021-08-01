f8925d460e9ce1c7607e3ad29fb681491368258b
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-01 18:23:27 +0100

    First tests passing on Room, Guest & Song

--- tests/test_room.py
+++ tests/test_room.py
@@ -1,18 +1,20 @@
 import unittest
 
-from tests.test_guest import TestGuest
-from tests.test_song import TestSong
-from tests.test_room import TestRoom
+from src.guest import Guest
+from src.song import Song
+from src.room import Room
 
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
+        # sample rooms
         self.room1 = Room("Compton Crib", 20, 0, "Hip Hop")
         self.room2 = Room("Soul Shack", 20, 0, "Soul")
         self.room3 = Room("Nashville Nook", 20, 0, "Country")
         self.room4 = Room("Pop Pad", 20, 0, "Chart")
         self.room5 = Room("School of Rock", 20, 0, "Rock")
 
+        # sample guests
         self.guest1 = Guest("Lil T", 23, 40, "Hip Hop")
         self.guest2 = Guest("Martha Vandell", 30, 25, "Soul")
         self.guest3 = Guest("Waylon Walton", 28, 30, "Country")
@@ -24,6 +26,7 @@ class TestRoom(unittest.TestCase):
         self.guest9 = Guest("Stan Cooke", 43, 40, "Soul")
         self.guest10 = Guest("Tanisha Ordell", 19, 35, "Hip Hop")
 
+        #sample songs
         self.song1 = Song("Poundin 40s", "Crenshaw Cru", "Hip Hop")
         self.song2 = Song("Shawty Shake", "Doctah Dolla", "Hip Hop")
         self.song3 = Song("Cryin an Drinkin", "Marv N", "Soul")
@@ -31,6 +34,12 @@ class TestRoom(unittest.TestCase):
         self.song5 = Song("Your Cheatin Horse", "Harvest Hogs", "Country")
         self.song6 = Song("Trailer Hoedown", "The Hogfather", "Country")
         self.song7 = Song("Black Nails", "Jane's Jetts", "Rock")
-        self.song8 = Song("Python", "SnakeLake", "Rock")
+        self.song8 = Song("Eye of the Python", "SnakeLake", "Rock")
         self.song9 = Song("Love Simp", "Nick Dastly", "Chart")
         self.song10 = Song("Girl don't hate me", "Hairgelled Hearthrobs", "Chart")
+
+
+    # Tests
+
+    def test_room_has_name(self):
+        self.assertEqual("Compton Crib", self.room1.name)
